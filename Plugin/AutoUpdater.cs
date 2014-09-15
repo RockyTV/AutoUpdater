d101e6868225e898d5fd417e6695d3d038dc3615
Alexandre Oliveira <rockytvbr@gmail.com>
2014-09-15 16:33:39 -0300

    Published on GitHub

---
+++ Plugin/AutoUpdater.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using DarkMultiPlayerServer;
+
+namespace AutoUpdater
+{
+    public class AutoUpdater : DMPPlugin
+    {
+        public AutoUpdater()
+        {
+            DarkLog.Normal("[AutoUpdater] AutoUpdater v" + Common.VERSION + " initiated!");
+        }
+
+        public override void OnUpdate()
+        {
+            Common.DoCheck();
+        }
+    }
+}
