27269c532273f3006d6134fe5a9e639f3444f816
Alexandre Oliveira <rockytvbr@gmail.com>
2014-10-13 10:08:49 -0300

    Updated to 1.0.1.5

---
+++ AutoUpdater/Log.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+
+namespace AutoUpdater
+{
+    public class Log
+    {
+        private static string logDir = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, Path.Combine("AutoUpdater", "logs"));
+        private static string logFile = Path.Combine(logDir, "autoupdater " + DateTime.Now.ToString("yyyy-MM-dd HH-mm-ss") + ".log");
+
+        public enum LogLevels
+        {
+            DEBUG,
+            INFO,
+            ERROR
+        }
+
+        private static void WriteLog(LogLevels level, string message)
+        {
+            if (!Directory.Exists(logDir))
+            {
+                Directory.CreateDirectory(logDir);
+            }
+
+            string output = "[" + DateTime.Now.ToString("HH:mm:ss") + "][" + level.ToString() + "] : " + message;
+            Console.WriteLine(output);
+            
+            try
+            {
+                File.AppendAllText(logFile, output + Environment.NewLine);
+            }
+            catch (Exception e)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("Error writing to log file!, Exception: " + e);
+                Console.ForegroundColor = ConsoleColor.Gray;
+            }
+        }
+
+        public static void Debug(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            WriteLog(LogLevels.DEBUG, message);
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+
+        public static void Normal(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Gray;
+            WriteLog(LogLevels.INFO, message);
+        }
+
+        public static void Error(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            WriteLog(LogLevels.ERROR, message);
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+    }
+}
--- AutoUpdater/Properties/AssemblyInfo.cs
+++ AutoUpdater/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("AutoUpdater")]
-[assembly: AssemblyDescription("Auto Updater plugin for DarkMultiPlayer")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("AutoUpdater")]
-[assembly: AssemblyCopyright("Copyright © 2014")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("acc9f976-91a4-470d-a348-ac85b592faa7")]
+[assembly: Guid("59c08ca3-a55e-4762-956d-4e9683903b14")]
 
 // Version information for an assembly consists of the following four values:
 //
